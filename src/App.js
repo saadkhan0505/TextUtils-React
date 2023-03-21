946bd6db92871c91fbd34830f7249aa2519363fe
saadkhan0505 <saadahmedkhan552000@gmail.com>
2023-03-21 16:23:59 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,77 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+
 
 
 function App() {
+  const [mode, setMode] = useState('light'); //will tells whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)  => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert('Dark mode has been enabled', "success")
+      document.title = "TextUtils - Dark Mode";
+
+      //Bad User Experience: Yeh sirf samjhane ke liye btaya ke title kese change hota rehta attention lene ke liye kch apps main 
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light mode has been enabled', "success")
+      document.title = "TextUtils - Light Mode"
+    }
+  }
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+   {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
+   {/* <Navbar /> */}
+   {/* <BrowserRouter> */}
+   <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+   <Alert alert = {alert}/>
+   <div className="container my-3">
+      {/* /users --> Component 1
+          /users/home --> Component 2
+      React path ko partial match krta hai isliye complete path matching ke liye exact use krte hai */}
+      
+      {/* <Routes>
+          <Route exact path="/about" element={<About />} />
+          
+          < Route exact path="/" element= {<TextForm heading="Enter the text to analyse: " mode={mode}/>} /> 
+      </Routes> */}
+
+   <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
+   {/* <About/> */}
+
+   </div>
+   {/* </BrowserRouter>  */}
     </>
   );
 }
